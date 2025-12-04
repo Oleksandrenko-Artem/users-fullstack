8760975290b9df6c1ebc0ced0d5c53aa9fb83b0f
Artem Oleksandrenko <artem2008.zp@gmail.com>
2025-12-04 15:29:55 +0200

    Add Loading users

--- client/src/components/UsersList/index.jsx
+++ client/src/components/UsersList/index.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import BeatLoader from 'react-spinners/BeatLoader';
 import styles from './UsersList.module.sass';
 import defImage from './defaultPhoto.jpg';
 
-export const UsersList = ({ users, isFetching, error }) => {
+export const UsersList = ({ isFetching, error }) => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data.data)).catch(err => console.log('error ====>', err));
+  }, []);
   return (
     <>
       <BeatLoader loading={isFetching} />
--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const { STATIC_PATH } = require('./constants');
 const { errorHandlers } = require('./middleware');
 const router = require('./routes');
 
 const app = express();
+
+const corsOptions = {
+    origin: '*',
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static(STATIC_PATH));
 
